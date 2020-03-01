ccb09dab263bb29a263d1085e6b3aa9b817fd65f
Wouter de Jong <wouter@wouterj.nl>
2020-03-01 11:22:25 +0100

    Added automatically CSRF protected authenticators

---
+++ src/Symfony/Component/Security/Http/EventListener/CsrfProtectionListener.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Security\Http\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Http\Authenticator\CsrfProtectedAuthenticatorInterface;
+use Symfony\Component\Security\Http\Event\VerifyAuthenticatorCredentialsEvent;
+
+class CsrfProtectionListener implements EventSubscriberInterface
+{
+    private $csrfTokenManager;
+
+    public function __construct(CsrfTokenManagerInterface $csrfTokenManager)
+    {
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [VerifyAuthenticatorCredentialsEvent::class => ['verifyCredentials', 256]];
+    }
+
+    public function verifyCredentials(VerifyAuthenticatorCredentialsEvent $event): void
+    {
+        $authenticator = $event->getAuthenticator();
+        if (!$authenticator instanceof CsrfProtectedAuthenticatorInterface) {
+            return;
+        }
+
+        $csrfToken = new CsrfToken($authenticator->getCsrfTokenId(), $authenticator->getCsrfToken($event->getPreAuthenticatedToken()->getCredentials()));
+        if (false === $this->csrfTokenManager->isTokenValid($csrfToken)) {
+            throw new InvalidCsrfTokenException('Invalid CSRF token.');
+        }
+    }
+}
